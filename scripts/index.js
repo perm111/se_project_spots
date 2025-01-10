9923204824f933adade70f748171bd8956d66a13
Igor <Igor@gmail.com>
2025-01-09 23:05:26 -0500

    Project  4 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,27 @@ const initialCards = [
   },
 ];
 
-const editModal = document.querySelector("#edit-Modal"); // The whole modal
+const profileName = document.querySelector(".profile__name"); //profile name
+const profileDescription = document.querySelector(".profile__description");
 const profileEditButton = document.querySelector(".profile__edit-btn"); // The pencil picture button
+
+const editModal = document.querySelector("#edit-Modal"); // The whole modal
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn"); //close button inside Modal
+const editModalNameInput = editModal.querySelector("#profile-name-input"); //modal profile name - search just the modal
+const editModalDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+); //modal profile name - search just the modal
+const editFormElement = editModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template"); // create a const to accept a card
+const cardsList = document.querySelector(".cards__list");
+
 
 function openModal() {
+  // Take the text content from the profile name
+  //and assign it to the value of the text input
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
+
   editModal.classList.add("modal_opened"); // add class to make modal visible
 }
 
@@ -37,5 +53,37 @@ function closeModal() {
   editModal.classList.remove("modal_opened"); // remove class to make modal invisible
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+
+  closeModal();
+}
+
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+function getCardElement(data) // get the element from an array
+{console.log("data:",data);
+  // access template and pull .card class
+
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  cardNameEl.textContent = data.name;
+
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = data.link;
+
+  return cardElement;
+}
+
+
+for (let i = 0; i < initialCards.length; i++)
+{const cardElement =  getCardElement(initialCards[i]);
+cardsList.prepend(cardElement);
+
+}
